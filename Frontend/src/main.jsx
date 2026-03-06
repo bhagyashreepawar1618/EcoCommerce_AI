aebe729d9d041b0d82c4fe39cd9e23130a8a0efb
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-06 17:29:43 +0530

    added proper routing for each component

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
+import Register from "./components/Register.jsx";
+import Login from "./components/Login.jsx";
+import ProductTagGenerator from "./components/ProductTagGenerator";
+import ImpactGenerator from "./components/ProductImpactGenerator";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "generate-tags",
+        element: <ProductTagGenerator />,
+      },
+      {
+        path: "generate-impact",
+        element: <ImpactGenerator />,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={route}></RouterProvider>,
+);
