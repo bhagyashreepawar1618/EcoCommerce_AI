e94d77db5975407da874e688d2b48ffa54e2c510
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-06 23:46:39 +0530

    added auth middleware to verify jwt

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import ApiError from '../utils/ApiError.js';
+import jwt from 'jsonwebtoken';
+import asyncHandler from '../utils/asyncHandler.js';
+import { User } from '../models/user.model.js';
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    console.log('token is from jwt =', token);
+
+    if (!token) {
+      throw new ApiError(401, 'Unauthorised Request');
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select('-password');
+
+    if (!user) {
+      throw new ApiError(401, 'Invalid AcessToken');
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, 'Invalid AccessToken');
+  }
+});
