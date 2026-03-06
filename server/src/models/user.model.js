ad757896c57a09e8accc63b06030a109c5cc81f5
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-06 19:34:54 +0530

    added user model to store user data

---
+++ server/src/models/user.model.js
@@ -0,0 +1,65 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      index: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+
+  //if password is modified
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+//is password correct method
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(this.password, password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model('User', userSchema);
