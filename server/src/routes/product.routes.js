92c966a75623bf20d94f3a28955df4ae6f1d4815
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-06 18:19:25 +0530

    added route for generate-impact

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { generateTags } from '../controllers/product.controller.js';
+import {
+  generateTags,
+  generateImpact,
+} from '../controllers/product.controller.js';
 
 const router = Router();
 
 router.route('/generate-tags').post(generateTags);
+router.route('/generate-impact').post(generateImpact);
 
 export default router;
