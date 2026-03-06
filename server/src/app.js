a629b01ddf5d5a7d13171b11caf09ffdaf82c6ef
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-07 01:33:57 +0530

    added temprary cors origin

--- server/src/app.js
+++ server/src/app.js
@@ -4,12 +4,7 @@ import userRouter from './routes/User.routes.js';
 import cors from 'cors';
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
