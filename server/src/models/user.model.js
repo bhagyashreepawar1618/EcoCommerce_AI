6fd6894453574d77844beef2a4faa94ff978e7e6
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-06 23:49:45 +0530

    fixed a small bug in user model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new Schema(
   {
@@ -33,7 +35,7 @@ userSchema.pre('save', async function () {
 
 //is password correct method
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(this.password, password);
+  return await bcrypt.compare(password, this.password);
 };
 
 userSchema.methods.generateAccessToken = function () {
