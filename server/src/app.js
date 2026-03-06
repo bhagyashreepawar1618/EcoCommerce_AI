0f933e3d4513a04a70a5f53fd0537d4dd413c66b
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-06 20:00:42 +0530

    added userRouter in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import productRouter from './routes/product.routes.js';
+import userRouter from './routes/User.routes.js';
 import cors from 'cors';
 const app = express();
 
@@ -13,5 +14,6 @@ app.use(
 app.use(express.json());
 
 app.use('/api/ai/products', productRouter);
+app.use('/api/v1/user', userRouter);
 
 export { app };
